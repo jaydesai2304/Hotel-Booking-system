adf2a0ebceb58417e5f02d70ac32926a93eba0a2
jaydesai2304 <jaydesai2304@gmail.com>
2024-05-13 11:37:00 +0530

    Add term & condition page

--- hotel_management/hotel/urls.py
+++ hotel_management/hotel/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('about/',views.about, name="about"),
     path('service/',views.service, name="service"),
     path('contact/',views.contact, name="contact"),
+    path('tc/',views.tc, name="tc"),
 
 ]
